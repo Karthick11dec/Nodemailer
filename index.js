281fc9bbc3bea7f304bced1228b492369769b0ae
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-02-24 12:58:00 +0530

    three steps added

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const nodemailer = require('nodemailer');
 
 // step 1
+
+const transporter = nodemailer.createTransport({
+	service: 'gamil',
+	auth: {
+		user: '',
+		pass: '',
+	},
+});
+
 // step 2
+
+let mailOptione = {
+	from: 'chilamakurvishnu@gmail.com',
+	to: 'visshnnutejaa@gmail.com',
+	subjet: 'Testing Nodemailer',
+	text: 'It works',
+};
+
 // step 3
+
+transporter.sendMail(mailOptione, (err, data) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('Email Sent');
+	}
+});
